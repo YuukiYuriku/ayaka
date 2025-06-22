2e6f82c9885b3fa2221ced645ca3c9954af10b44
YuukiYuriku <devan051003@gmail.com>
2025-06-22 16:55:18 +0700

    feat: implement repository interface for tblmastervendor with CRUD operations

---
+++ internal/domain/tblmastervendor/repository.go
@@ -0,0 +1,14 @@
+package tblmastervendor
+
+import (
+	"context"
+
+	"gitlab.com/ayaka/internal/pkg/pagination"
+)
+
+type Repository interface {
+	Create(ctx context.Context, data *Create) (*Create, error)
+	Fetch(ctx context.Context, name, cat string, param *pagination.PaginationParam) (*pagination.PaginationResponse, error)
+	Detail(ctx context.Context, vendorCode string) (*Detail, error)
+	Update(ctx context.Context, data *Update) (*Update, error)
+}
